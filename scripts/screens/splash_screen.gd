e1bdefdd763379dd07cf407e4ec13920e31a60c0
reshadf <reshadfar@gmail.com>
2026-04-16 23:37:45 +0200

    Debug splash screen

--- scripts/screens/splash_screen.gd
+++ scripts/screens/splash_screen.gd
@@ -1,11 +1,8 @@
 extends CanvasLayer
 
 func _ready() -> void:
-	print("Splash: starting...")
-	await get_tree().create_timer(2.0).timeout
-	print("Splash: timer done, loading main_menu...")
-	var result = get_tree().change_scene_to_file("res://scenes/screens/main_menu.tscn")
-	if result != OK:
-		print("Splash: FAILED to load main_menu, error code: ", result)
-	else:
-		print("Splash: main_menu loaded successfully")
+	print("Splash: _ready() called!")
+	await get_tree().create_timer(1.0).timeout
+	print("Splash: timer fired!")
+	var r = get_tree().change_scene_to_file("res://scenes/screens/main_menu.tscn")
+	print("Splash: change_scene result = ", r)
