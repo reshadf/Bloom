7afd22dfcc78051a1c139ecbb11df02383e59f9c
reshadf <reshadfar@gmail.com>
2026-04-16 23:42:21 +0200

    Simplify splash

--- scripts/screens/splash_screen.gd
+++ scripts/screens/splash_screen.gd
@@ -1,8 +1,5 @@
 extends CanvasLayer
 
-func _ready() -> void:
-	print("Splash: _ready() called!")
-	await get_tree().create_timer(1.0).timeout
-	print("Splash: timer fired!")
-	var r = get_tree().change_scene_to_file("res://scenes/screens/main_menu.tscn")
-	print("Splash: change_scene result = ", r)
+func _ready():
+	print("SPLASH: HELLO")
+	get_tree().change_scene_to_file("res://scenes/screens/main_menu.tscn")
